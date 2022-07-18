05c5d0c4a21c62c238025680ec3602f1e3816bbe
Paul Arinze <87880495+paularinzee@users.noreply.github.com>
2022-07-18 14:41:57 -0700

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a388f4041a49a4a178a1056b86aca723-112383105.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
